bf12e56907ac63876dc32fc1f946c02b3da913c2
App Academy Student <student@appacademy.io>
2017-04-06 14:11:16 -0700

    Add sub_sums methods

--- execution_time_diffs.rb
+++ execution_time_diffs.rb
@@ -36,40 +36,48 @@ def my_min2(list)
 end
 p my_min2([0, 3, 5, 4, -5, 10, 1, 90])
 
-# phase 1 : quadratic : O(n**2)
+# phase 1 : cubic : O(n**3)
 
-# def sub_sums(array)
-#   subs = []
-#
-#   array.each_with_index do |el, idx|
-#     # sub_arr = array[0...idx] + array[idx + 1..-1]
-#     subs << [el]
-#     array.each_with_index do |el2, idx2|
-#       next if idx == idx2
-#       subs << [el, el2]
-#     end
-#   end
-#   subs << [array]
-# end
 
-def sub_sums(array)
-  return [[]] if array.empty?
-  subs = sub_sums(array[0...-1])
-  subs = subs.concat(subs.map { |x| x.dup << array[-1] })
-  subs = subs.select do |arr|
-    arr.each_cons(2).all? { |a,b| (a + 1) == b }
+def sub_sums1(array)
+  subs = []
+
+  array.each_index do |idx1|
+    (idx1..array.length - 1).each do |idx2|
+      subs << array[idx1..idx2]
+    end
   end
 
-  sum(subs)
+  subs.map { |sub| sub.reduce(:+) }.max
+
 end
 
-def sum(arr)
-  arr.reject! { |arr| arr.empty? }
+p sub_sums1([2, 3, -6, 7, -6, 7])
 
-  arr.map! { |arr| arr.reduce(:+) }
+#phase 2: 0(n)
 
-  arr.max
+def sub_sums2(arr)
+  largest_sum = arr.max
+
+  pivot_idx = arr.index(largest_sum)
+
+  if arr.drop(pivot_idx).reduce(:+) > largest_sum
+    largest_sum = arr.drop(pivot_idx).reduce(:+)
+  end
+
+  if pivot_idx == 0
+    if arr.take(pivot_idx + 2).reduce(:+) > largest_sum
+      largest_sum = arr.take(pivot_idx + 2).reduce(:+)
+    end
+  else
+    if arr.take(pivot_idx + 1).reduce(:+) > largest_sum
+      largest_sum = arr.take(pivot_idx + 1).reduce(:+)
+    end
+  end
+
+  largest_sum
 end
 
-
-p sub_sums([2, 3, -6, 7, -6, 7])
+p sub_sums2([2, 3, -6, 7, -6, 7])
+p sub_sums2([7, 6, -1, -5, 7, 5])
+p sub_sums2([-5, -1, -3])
