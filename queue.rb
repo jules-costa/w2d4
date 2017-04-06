9722013d3451d2ead8eeb956513fe5fc0042d722
App Academy Student <student@appacademy.io>
2017-04-06 16:52:31 -0700

    Add anangrams

---
+++ queue.rb
@@ -0,0 +1,22 @@
+class Queue
+
+  def initialize
+    @store = []
+  end
+
+  def enqueue
+  end
+
+  def dequeue
+  end
+
+  def peek
+  end
+
+  def size
+  end
+
+  def empty?
+  end
+
+end
---
+++ stack.rb
@@ -0,0 +1,22 @@
+class Stack
+
+  def initialize
+    @store = []
+  end
+
+  def pop
+  end
+
+  def push
+  end
+
+  def peek
+  end
+
+  def size
+  end
+
+  def empty?
+  end
+
+end
